7e368c5381920ad9585142b240cf1b94516f8c97
MartinFernandezSantia <101137024+MartinFernandezSantia@users.noreply.github.com>
2024-08-26 19:05:49 -0300

    Finalizado login de usuarios en Auth

--- src/modelo/Auth.java
+++ src/modelo/Auth.java
@@ -1,14 +1,73 @@
 package modelo;
 
 import org.mindrot.jbcrypt.BCrypt;
+import clase.Usuario;
+
+import javax.xml.crypto.Data;
+import java.sql.*;
 
 public class Auth {
 
+    /**
+     * Hashea un String con algoritmo BCrypt.
+     * @param password String para hashear.
+     * @return password hasheada.
+     */
     public static String hashPassword(String password) {
         return BCrypt.hashpw(password, BCrypt.gensalt());
     }
 
+    /**
+     * @param password String plano.
+     * @param hashed String previamente hasheado.
+     * @return true si las contraseña coinciden, sino false.
+     */
     public static Boolean checkPassword(String password, String hashed) {
         return BCrypt.checkpw(password, hashed);
+    }
+
+    /**
+     * Autentifica al usuario y retorna una instancia del mismo si los datos son correctos.
+     * @param legajo n° de legajo del usuario.
+     * @param password contraseña del usuario.
+     * @return Instancia de Usuario o null si los datos no son correctos.
+     */
+    public static Usuario login(int legajo, String password) {
+        String getUsusarioSQL = "SELECT * FROM usuarios WHERE legajo = ?";
+
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(getUsusarioSQL);
+             Statement stmt = conn.createStatement()) {
+
+            // Uso la DB
+            stmt.execute("USE hpc_db");
+
+            // Obtengo al usuario en la DB
+            pstmt.setInt(1, legajo);
+            ResultSet resultUsuario = pstmt.executeQuery();
+
+            // Si el usuario existe
+            if (resultUsuario.next()) {
+                String dbPassword = resultUsuario.getString("contraseña");
+
+                // Si la contraseña es la correcta
+                if (checkPassword(password, dbPassword)) {
+                    int id = resultUsuario.getInt("id");
+                    String nombre = resultUsuario.getString("nombre");
+                    String apellido = resultUsuario.getString("apellido");
+
+                    return new Usuario(id, nombre, apellido, legajo, dbPassword);
+                }
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(login(12345, "ASDasd123"));
     }
 }
--- src/modelo/DatabaseConnection.java
+++ src/modelo/DatabaseConnection.java
@@ -10,6 +10,8 @@ public class DatabaseConnection {
     private static final String USER = "root";
     private static final String PASSWORD = "ASDasd123";
 
+    public static final String USE = "hpc_db";
+
     public static Connection getConnection() throws SQLException{
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
