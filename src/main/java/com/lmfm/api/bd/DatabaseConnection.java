a8134833d308c8aff0734162eea9e03330e3b33d
MartinFernandezSantia <martin-santia@hotmail.com.ar>
2025-12-11 21:23:50 -0300

    Reinstated env vars in connection

--- src/main/java/com/lmfm/api/bd/DatabaseConnection.java
+++ src/main/java/com/lmfm/api/bd/DatabaseConnection.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://root:ikkLjQWdirRYHcwpwNtnaMWPsUtmmkZl@crossover.proxy.rlwy.net:35586/railway";
-    private static final String USER = "root";
-    private static final String PASSWORD = "ikkLjQWdirRYHcwpwNtnaMWPsUtmmkZl";
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     // private static final String URL = "jdbc:mysql://localhost:3306/hpc_db";
     // private static final String USER = "root";
@@ -16,7 +16,7 @@ public class DatabaseConnection {
 
 
     public static Connection getConnection() throws SQLException{
-        return DriverManager.getConnection(URL);
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 
 }
